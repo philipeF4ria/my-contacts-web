f744e7e65f45811bc1f5605ab0e5a32508c96fcf
Philipe Faria <philipefaria61@gmail.com>
2021-12-16 15:03:21 -0300

    Creating PageHeader, Input, Select and Button components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,23 @@
+import PageHeader from '../../components/PageHeader';
+
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
 export default function NewContact() {
-  return 'NewContact';
+  return (
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input placeholder="Nome"/>
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button type="button">
+        Salvar Alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar Alterações
+      </Button>
+    </>
+  );
 }
